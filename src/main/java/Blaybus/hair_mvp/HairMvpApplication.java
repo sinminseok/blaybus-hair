23f44fd6e3e81b9c5aae1a1708a70ca281daef2b
sinminseok12@naver.com <55183314+sinminseok@users.noreply.github.com>
2025-02-11 00:15:07 +0900

    init

---
+++ src/main/java/Blaybus/hair_mvp/HairMvpApplication.java
@@ -0,0 +1,13 @@
+package Blaybus.hair_mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HairMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HairMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Blaybus/hair_mvp/HairMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package Blaybus.hair_mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HairMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
