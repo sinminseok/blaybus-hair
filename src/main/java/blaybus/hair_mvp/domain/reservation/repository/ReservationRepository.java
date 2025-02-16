73d5db712d2a1b4e302e84817e89ba173be955a4
WooyoungKwon <rnjsdndud00@naver.com>
2025-02-16 13:19:27 +0900

    feat(reservation): 유저 예약 내역 조회 (status 값 추가 필요)

--- src/main/java/blaybus/hair_mvp/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/blaybus/hair_mvp/domain/reservation/repository/ReservationRepository.java
@@ -1,6 +1,9 @@
 package blaybus.hair_mvp.domain.reservation.repository;
 
 import blaybus.hair_mvp.domain.reservation.entity.Reservation;
+import jakarta.persistence.Entity;
+import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,6 @@ import java.util.UUID;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+    @EntityGraph(attributePaths = {"designer"})
+    List<Reservation> findByUser_Id(UUID userId);
 }
--- src/main/java/blaybus/hair_mvp/domain/reservation/service/ReservationService.java
+++ src/main/java/blaybus/hair_mvp/domain/reservation/service/ReservationService.java
@@ -3,12 +3,15 @@ package blaybus.hair_mvp.domain.reservation.service;
 import blaybus.hair_mvp.domain.designer.entity.Designer;
 import blaybus.hair_mvp.domain.designer.repository.DesignerRepository;
 import blaybus.hair_mvp.domain.reservation.dto.ReservationRequest;
+import blaybus.hair_mvp.domain.reservation.dto.ReservationResponse;
 import blaybus.hair_mvp.domain.reservation.entity.Reservation;
 import blaybus.hair_mvp.domain.reservation.mapper.ReservationMapper;
 import blaybus.hair_mvp.domain.reservation.repository.ReservationRepository;
 import blaybus.hair_mvp.domain.user.entity.User;
 import blaybus.hair_mvp.domain.user.repository.UserRepository;
 import blaybus.hair_mvp.utils.OptionalUtil;
+import java.util.List;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -28,7 +31,8 @@ public class ReservationService {
     @Transactional
     public void save(final ReservationRequest request, final String userEmail) {
         Reservation reservation = reservationMapper.toEntity(request);
-        Designer designer = OptionalUtil.getOrElseThrow(designerRepository.findById(UUID.fromString(request.getDesignerId())), "존재하지 않는 디자이너 ID 입니다.");
+        Designer designer = OptionalUtil.getOrElseThrow(
+                designerRepository.findById(UUID.fromString(request.getDesignerId())), "존재하지 않는 디자이너 ID 입니다.");
         User user = OptionalUtil.getOrElseThrow(userRepository.findByEmail(userEmail), "존재하지 않는 사용자 ID 입니다.");
         reservation.setDesigner(designer);
         reservation.setUser(user);
@@ -37,4 +41,18 @@ public class ReservationService {
         user.addReservation(reservation);
     }
 
+    public List<ReservationResponse> findReservationsByUserId(UUID userId) {
+        List<Reservation> reservations = reservationRepository.findByUser_Id(userId);
+        // status
+        return reservations.stream().map(
+                reservation -> ReservationResponse.builder()
+                        .id(reservation.getId())
+                        .designerName(reservation.getDesigner().getName())
+                        .shopAddress(reservation.getDesigner().getShopAddress())
+                        .meetingType(reservation.getMeetingType())
+                        .price(reservation.getPrice())
+                        // status 추가 예정
+                        .build()
+        ).toList();
+    }
 }
